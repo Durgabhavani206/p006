debac6b3ebaaee3a4aefc177190e9c3b3edfee5c
Durgabhavani206 <punyamanthulabhavani2006@gmail.com>
2025-08-06 09:08:28 +0530

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-
 import React, { Component } from 'react';
 import './App.css';
 class App extends Component {
   render() {
+    const BASEURL = import.meta.env.BASE_URL; /*it will take repository name from vite.config.js.we are using this because when I click on*/
+
+       /*link in guthub it will not show images,so we use the above for  to get images*/
     return (
       <>
         <header>
@@ -10,39 +12,39 @@ class App extends Component {
         </header>
         <section>
           <div className='card'>
-            <img src='/1.jpeg' alt=''/>
+            <img src={`${BASEURL}1.jpeg`} alt='' />  
             <label>Image 01</label>
           </div>
-           <div className='card'>
-            <img src='/2.jpeg' alt=''/>
+          <div className='card'>
+            <img src={`${BASEURL}2.jpeg`} alt='' />
             <label>Image 02</label>
           </div>
-           <div className='card'>
-            <img src='/3.jpeg' alt=''/>
+          <div className='card'>
+            <img src={`${BASEURL}3.jpeg`} alt='' />
             <label>Image 03</label>
           </div>
-           <div className='card'>
-            <img src='/4.jpeg' alt=''/>
+          <div className='card'>
+            <img src={`${BASEURL}4.jpeg`} alt='' />
             <label>Image 04</label>
           </div>
-           <div className='card'>
-            <img src='/5.jpeg' alt=''/>
+          <div className='card'>
+            <img src={`${BASEURL}5.jpeg`} alt='' />
             <label>Image 05</label>
           </div>
-           <div className='card'>
-            <img src='/6.jpeg' alt=''/>
+          <div className='card'>
+            <img src={`${BASEURL}6.jpeg`} alt='' />
             <label>Image 06</label>
           </div>
-           <div className='card'>
-            <img src='/7.jpeg' alt=''/>
+          <div className='card'>
+            <img src={`${BASEURL}7.jpeg`} alt='' />
             <label>Image 07</label>
           </div>
-           <div className='card'>
-            <img src='/8.jpeg' alt=''/>
+          <div className='card'>
+            <img src={`${BASEURL}8.jpeg`} alt='' />
             <label>Image 08</label>
           </div>
-           <div className='card'>
-            <img src='/9.jpeg' alt=''/>
+          <div className='card'>
+            <img src={`${BASEURL}9.jpeg`} alt='' />
             <label>Image 09</label>
           </div>
         </section>
